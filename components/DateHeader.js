021cffc8ddb5b1c059f3a689a6bb969202cca1a2
Ahmad Luqman <ahmad.luqman@gmail.com>
2019-04-14 20:37:41 +0500

    fix typo in DateHeader and yarn upgrade

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-export default function ({ date }) {
+export default function DateHeader ({ date }) {
   return (
     <Text>
       {date}
